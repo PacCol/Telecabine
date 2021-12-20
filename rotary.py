2877a10e131a94b9540035e000dc67585c05eaec
PacCol <pacome.colinet1@gmail.com>
2021-12-20 23:14:19 +0100

    update

--- rotary.py
+++ rotary.py
@@ -1,50 +1,31 @@
-from RPi import GPIO
-from time import sleep
-from threading import Thread
+import gpiozero
 
 import __main__
 
-def rotary():
+rotor = gpiozero.RotaryEncoder(15, 18, wrap=False, max_steps=5)
+rotor.steps = __main__.speed - 5
 
-    global __main__
-
-    clk = 18
-    dt = 15
-    sw = 14
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    clkLastState = GPIO.input(clk)
-
-    while True:
-        
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
-        swState = GPIO.input(sw)
-
-        if swState != 1:
-            if __main__.enabled:
-                __main__.changeStatus(False, __main__.speed)
-            else:
-                __main__.changeStatus(True, __main__.speed)
-            sleep(0.3)
-
-        if clkState != clkLastState and clkState == 1:
-            if dtState == clkState:
-                if __main__.speed < 10:
-                    __main__.changeStatus(__main__.enabled, __main__.speed + 1)
-            else:
-                if __main__.speed > 1:
-                    __main__.changeStatus(__main__.enabled, __main__.speed - 1)
-
-            sleep(0.15)
-
-        clkLastState = clkState
-        sleep(0.001)
+rotaryButton = gpiozero.Button(14)
 
 
-rotaryThread = Thread(target=rotary, args=())
-rotaryThread.start()
+def changeSpeed():
+    selectedSpeed = rotor.steps + 5
+
+    print(selectedSpeed)
+
+    if selectedSpeed == 0:
+        rotor.steps = selectedSpeed - 5
+        __main__.changeStatus(False, 1)
+    else:
+        __main__.changeStatus(__main__.enabled, selectedSpeed)
+
+
+def toggleStatus():
+    if __main__.enabled:
+        __main__.changeStatus(False, __main__.speed)
+    else:
+        __main__.changeStatus(True, __main__.speed)
+
+
+rotor.when_rotated = changeSpeed
+rotaryButton.when_released = toggleStatus
