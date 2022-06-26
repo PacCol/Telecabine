81f2f24924a73c1821fec2ec041a00fce1be7fe5
PacCol <pacome.colinet1@gmail.com>
2022-06-26 18:09:03 +0200

    updated

--- update.py
+++ update.py
@@ -1,40 +1,56 @@
 import os
-import urllib.request
+import sys
 import git
+import subprocess
 
 repo = git.Repo("./")
 
 def updateCode():
-    if isConnected():
+    try:
         ret = repo.remotes.origin.pull()
-        print(ret[0].flags)
         if ret[0].flags == 4:
             return "No changes"
         else:
             return "Updated"
-    else:
+    except:
         return "Network unreachable"
 
 def updateModules():
-    if isConnected():
-        os.system("sudo apt-get install python3-pip")
-        os.system("sudo pip3 install GitPython")
-        os.system("sudo pip3 install luma.oled")
-        os.system("sudo pip3 install flask")
-        os.system("sudo pip3 install flask_sqlalchemy")
-        repo = git.Repo("./static/RealCSS")
-        repo.remotes.origin.pull()
-        return "Updated"
+    ret = pipInstall("GitPython luma.oled flask flask_sqlalchemy")
+    if ret == "Success":
+        realCSSRepo = git.Repo("./static/RealCSS")
+        try:
+            realCSSRepo.remotes.origin.pull()
+            return "Success"
+        except:
+            return "Network unreachable"
+    elif ret == "Network unreachable":
+        return "Network unreachable"
+
+def pipInstall(module):
+    proc = subprocess.Popen("sudo pip3 install --timeout 1000 " + module, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while proc.poll() is None:
+        #print(proc.stdout.readline())
+        pass
+    commandResult = proc.wait()
+    if commandResult == 0:
+        return "Success"
     else:
         return "Network unreachable"
 
-def isConnected(host='http://github.com'):
-    try:
-        urllib.request.urlopen(host)
-        return True
-    except:
-        return True
-
 if __name__ == "__main__":
-    print(updateCode())
-    #print(updateModules())
+    print("Updating code...")
+    ret = updateCode()
+    if ret == "No changes":
+        print("No updates available")
+    elif ret == "Updated":
+        print("Code updated")
+        print("Updating modules, make sure your internet connection is stable...")
+        _ret = updateModules()
+        if _ret == "Success":
+            print("Modules updated")
+            print("Now, you can restart the device.")
+        elif _ret == "Network unreachable":
+            print("Network error, please retry now to avoid any corruption!")
+    elif ret == "Network unreachable":
+        print("Network error, try again later")
