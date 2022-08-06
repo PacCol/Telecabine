8e6d6fbdec5c2d037df750096f9b1e280890751e
PacCol <50033325+PacCol@users.noreply.github.com>
2022-08-06 16:29:02 +0200

    Heure ajoutée

--- hardware.py
+++ hardware.py
@@ -232,7 +232,7 @@ def displayStatusDaemon():
         if display.getLastReload() + timedelta(minutes=1) < now and display.isSleeping == False:
             if display.getCurrentScreen() == "Home":
                 display.displayStatus(motors.getSpeed(), lights.getStatus())
-            elif display.getCurrentScreen() == "Settings" and display.getCurrentSetting() == "General":
+            elif display.getCurrentScreen() == "Settings" and (display.getCurrentSetting() == "General" or display.getCurrentSetting() == "About"):
                 display.displaySettings(False, None, None)
 
         sleep(2)
