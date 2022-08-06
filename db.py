1305691702e1b1cd64f5990e608aed3b0c802ae6
PacCol <50033325+PacCol@users.noreply.github.com>
2022-08-06 17:29:59 +0200

    Bug de l'heure corrigé

--- db.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-from __main__ import app
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-db = SQLAlchemy(app)
